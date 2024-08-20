ea16fa15d3e1d5440f162df871efee6e34fc6b50
tomSeng <tom.seng@epitech.eu>
2024-08-20 09:28:20 +0200

    [CORE] changed the color to_hexa() function to be in the utils.cpp instead of the utils.h

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,3 +7,16 @@ Coordinates Coordinates::operator+=(Coordinates coord)
     z += coord.z;
     return *this;
 }
+
+Color Color::from_hexa(std::string hexcolor)
+{
+    int index = 0;
+    Color color;
+
+    if (hexcolor.size() != 6)
+        return Color();
+    color.r = hexcolor[0] * 16 + hexcolor[1];
+    color.g = hexcolor[2] * 16 + hexcolor[3];
+    color.b = hexcolor[4] * 16 + hexcolor[5];
+    return color;
+}
